aa7bb4ecd9c658a581cb9ab7a746344d485a236b
Yashwanth-Karumanchi <yashwanthkarumanchi@gmail.com>
2024-09-19 04:02:42 -0600

    Modified shell script

--- DecisionTree/run.sh
+++ DecisionTree/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if python3 -c "import tabulate" >/dev/null 2>&1; then
-    echo "Tabulate library is already installed."
+    echo "Tabulate librarclsy is already installed."
 else
     echo "Tabulate library is not installed. Installing now..."
     pip3 install tabulate
@@ -14,4 +14,4 @@ echo "Training and Test errors for bank dataset"
 python3 decisionTree_bank.py
 
 echo "Training and Test errors for bank dataset with unknown replaced with most Common Values"
-python3 decisionTree_bank_unknown.p
+python3 decisionTree_bank_unknown.py
